aa0b64e204c46ac64f70ed3ff6603c13b0023e50
wetootew <x.x@aol.it>
2014-04-10 18:47:23 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-if (!mysql_select_db('uchef', $link = mysql_connect('toprated.mysql.eu1.frbit.com', 'toprated', 'aTSRmyHGo0xqrzDV')))
+if (!mysql_select_db('toprated', $link = mysql_connect('toprated.mysql.eu1.frbit.com', 'toprated', 'aTSRmyHGo0xqrzDV')))
     echo '<p> db error ' . mysql_errno() . ": " . mysql_error() . '</p>';
 ?>
