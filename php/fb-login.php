5b91bb11b53737939379aa3b4efe74e6dbac3bde
wetootew <x.x@aol.it>
2014-04-22 16:52:35 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -5,5 +5,9 @@
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/it_IT/all.js#xfbml=1&appId=1439231382984557";
   fjs.parentNode.insertBefore(js, fjs);
+FB.api('/me', function (response) {
+alert('Welcome, ' + response.name + "!");
+alert('Your email id is : '+ response.email);
 }(document, 'script', 'facebook-jssdk'));</script>
 <div class="fb-login-button" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="true"></div>
+
