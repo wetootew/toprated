125dbf0969d20ec7f254f66858f3b76a25576f2c
wetootew <x.x@aol.it>
2014-05-16 23:54:41 +0200

    Update prova.php

--- php/prova.php
+++ php/prova.php
@@ -4,9 +4,9 @@ require_once('Oauth.php');
 
 session_start();
 
-$app_id = 'APP_ID';
-$app_secret = 'APP_SECRET';
-$callback = 'CALLBACK_URL'; // [DOMAIN]/auth/facebook.php in this example
+$app_id = '1439231382984557';
+$app_secret = '0a6b44656cebac45c3c6f4fd62aabbca';
+$callback = 'prova.php'; // [DOMAIN]/auth/facebook.php in this example
 
 $facebook = new Facebook($app_id, $app_secret, $callback);
 if($facebook->validateAccessToken()){
