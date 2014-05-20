db9cf9b8e47322f86ec9aebea343d9d5e62da656
wetootew <x.x@aol.it>
2014-05-20 11:54:01 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -1,9 +1,33 @@
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '{your-app-id}',
+          xfbml      : true,
+          version    : 'v2.0'
+        });
+      };
+      
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+function statusChangeCallback(response) {
+    if (response.status === 'connected') 
+      FB.api('/me', function(response) {
+        alert('Successful login for: ' + response.name);
+      });
+    else if (response.status === 'not_authorized') 
+      alert("not authorized");
+    else 
+      alert("not logged in");
+  }
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&appId=1439231382984557&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-login-button" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="false"></div>
+<div class="fb-login-button" scope="public_profile,email" onlogin="checkLoginState();" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="false"></div>
