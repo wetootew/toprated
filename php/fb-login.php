8f39e4d794f54861f437cd1de81d8a213078824c
wetootew <x.x@aol.it>
2014-05-20 11:05:56 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -3,11 +3,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/it_IT/all.js#xfbml=1&appId=1439231382984557";
+  js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&appId=1439231382984557&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
-FB.api('/me', function (response) {
-alert('Welcome, ' + response.name + "!");
-alert('Your email id is : '+ response.email);
 }(document, 'script', 'facebook-jssdk'));</script>
 <div class="fb-login-button" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="true"></div>
-
