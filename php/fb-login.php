2f1a23703518e458a7a140338dc5346e4e1b5c70
wetootew <x.x@aol.it>
2014-05-20 11:17:14 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -6,4 +6,4 @@
   js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&appId=1439231382984557&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-login-button" data-max-rows="1" data-size="small" data-show-faces="false" data-auto-logout-link="true"></div>
+<div class="fb-login-button" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="false"></div>
