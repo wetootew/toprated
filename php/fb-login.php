49cd272892df833214d772867ae6c2c0efcc0c6f
wetootew <x.x@aol.it>
2014-05-30 13:51:51 +0200

    Update fb-login.php
    [trigger:composer]

--- php/fb-login.php
+++ php/fb-login.php
@@ -31,7 +31,6 @@ function statusChangeCallback(response) {
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <?php
-echo 'we';
 session_start();
 use Facebook\FacebookJavaScriptLoginHelper;
 use Facebook\FacebookSession;
