563f2285dd8346bf888d7ba95485c239c2cbbb60
wetootew <x.x@aol.it>
2014-05-30 14:34:03 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -32,14 +32,7 @@ function statusChangeCallback(response) {
 }(document, 'script', 'facebook-jssdk'));</script>
 <?php
 session_start(); 
-require_once( 'Facebook/FacebookSession.php' );
-require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
-require_once( 'Facebook/FacebookRequest.php' );
-require_once( 'Facebook/FacebookResponse.php' );
-require_once( 'Facebook/FacebookSDKException.php' );
-require_once( 'Facebook/FacebookRequestException.php' );
-require_once( 'Facebook/FacebookAuthorizationException.php' );
-require_once( 'Facebook/GraphObject.php' );
+require 'vendor/autoload.php';
 use Facebook\FacebookJavaScriptLoginHelper;
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
