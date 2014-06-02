c269fedc5b2d5fe4ff3007921a8fd0fd20cca8ec
wetootew <x.x@aol.it>
2014-06-02 13:14:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,39 @@ include('db.php');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication('1439231382984557', '0a6b44656cebac45c3c6f4fd62aabbca');
+
+// Use one of the helper classes to get a FacebookSession object.
+//   FacebookRedirectLoginHelper
+//   FacebookCanvasLoginHelper
+//   FacebookJavaScriptLoginHelper
+// or create a FacebookSession with a valid access token:
+$helper = new FacebookJavaScriptLoginHelper();
+try {
+    $session = $helper->getSession();
+} catch(FacebookRequestException $ex) {
+    // When Facebook returns an error
+} catch(\Exception $ex) {
+    // When validation fails or other local issues
+}
+// Get the GraphUser object for the current user:
+
+try {
+  $me = (new FacebookRequest(
+    $session, 'GET', '/me'
+  ))->execute()->getGraphObject(GraphUser::className());
+  echo $me->getName();
+} catch (FacebookRequestException $e) {
+  // The Graph API returned an error
+} catch (\Exception $e) {
+  // Some other error occurred
+}
 ?>
 
 <!DOCTYPE html> 
