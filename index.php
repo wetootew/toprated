6def05619d70e86cc98580c8aeac87d43ff96481
wetootew <x.x@aol.it>
2014-06-08 21:30:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include('db.php'); 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+require_once __DIR__ . '/vendor/autoload.php';
 
 echo file_get_contents (__DIR__ . '/vendor/autoload.php');
 
