d7c25586f6dd8cbc104e1957c11635272818feab
wetootew <x.x@aol.it>
2014-06-17 21:01:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+include('db.php'); 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once __DIR__ . '/vendor/autoload.php';
+?>
 <!DOCTYPE html> 
 <title>Boh</title>
 <link rel="stylesheet" type="text/css" href="style.css">
