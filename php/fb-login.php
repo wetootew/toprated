46129c3de4cf0a6d992baba2a0e1e64d2c0ce5ac
wetootew <x.x@aol.it>
2014-08-19 15:25:46 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -43,3 +43,5 @@ if ($session) {
 } else
   echo "not logged in!";
 ?>
+  <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true"></div>
+
