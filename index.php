aebec476abd85a800f91285966676e3888c11963
wetootew <x.x@aol.it>
2014-08-19 15:31:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ jQuery(function($) {
  <img alt="" width=200 height=80 class=registered id=spaziodx src="logo.jpg">
  
  <div class="login unregistered">
-  <div style=float:right;><?php include('php/fb-login.php'); ?> <?php include('php/g-login.php'); ?></div>
+  <div style="float:right;"><?php include('php/fb-login.php'); ?> <?php include('php/g-login.php'); ?></div>
   <p> Sei registrato? </p>
   <input type=text name=usr placeholder=Username><br>
   <input type=password name=pwd placeholder=Password ><br>
