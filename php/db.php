5d8cccd522f6b55675d96f2d3ffce5b1458a6690
wetootew <x.x@aol.it>
2014-08-27 00:09:27 +0200

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1653,11 +1653,11 @@ class DB
 } 
 
 $db = new DB('toprated', 'toprated.mysql.eu1.frbit.com', 'toprated', '0AvbH5SIH186Bn6Q');
-$sql = "INSERT INTO test (Color, Age) Values ('Red', 7)"; 
-if (! $db->Query($sql)) { 
-echo "query failed"; 
-    $db->Kill(); 
-} 
+//$sql = "INSERT INTO test (Color, Age) Values ('Red', 7)"; 
+//if (! $db->Query($sql)) { 
+//echo "query failed"; 
+//    $db->Kill(); 
+//} 
 
-echo "Last ID inserted was: " . $db->GetLastInsertID(); 
+//echo "Last ID inserted was: " . $db->GetLastInsertID(); 
 ?>
