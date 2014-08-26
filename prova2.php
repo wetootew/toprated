46abfb0b2c298b020fcba7d182bbb8c03c79c5bd
wetootew <x.x@aol.it>
2014-08-26 12:40:01 +0200

    Create prova2.php

---
+++ prova2.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script>
+$(document).ready(function() {
+  var FBobject = {
+    loaded: false,
+    id: "",
+    email: "",
+    name: ""
+  };
+ 
+  $.ajaxSetup({ cache: true });
+  $.getScript(‘/connect.facebook.net/zh_TW/sdk.js’, function(){
+    FB.init({
+      appId      : ‘1439231382984557‘,
+      status     : true,
+      cookie     : true,
+      xfbml      : true,
+      version    : ‘v2.0′
+    });
+ 
+    FB.getLoginStatus(statusChangeCallback);
+    FB.Event.subscribe(‘auth.login’, statusChangeCallback);
+  });
+ 
+  function statusChangeCallback(response) {
+    if (response.status === ‘connected’) {
+      userLogin();
+    } else if (response.status === ‘not_authorized’) {
+      console.log(‘Please log into this app.’);
+    } else {
+      console.log(‘Please log into Facebook.’);
+   }
+  }
+ 
+  function userLogin() {
+    console.log(‘Welcome!Fetching your information…. ‘);
+    FB.api(‘/me’, function(response) {
+      FBobject.loaded = true;
+      FBobject.id = response.id;
+      FBobject.email = response.email;
+      FBobject.name = response.name;
+      console.log(FBobject);
+    });
+  }
+ 
+  $("#fb_login").bind("click", function (e) {
+    e.preventDefault();
+    if(FBobject.loaded==false){
+      FB.login(function(response) {
+        if (response.authResponse) {
+        }else{
+          alert("login");
+        }
+      }, {scope: ‘email,publish_actions’});
+    }else{
+      alert("no login");
+    }
+  });
+});
+</script>
+</head>
+<body>
+
+<button id="fb_login">Fb Login</button>
+
+<div id="fb-root"></div>
+</body>
+</html>
