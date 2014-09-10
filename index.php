110865a703321cc8db830ee9d22e1a655fdb464d
Nessus87 <nessus87@yahoo.it>
2014-09-10 21:13:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -152,56 +152,33 @@ jQuery(function($) {
 </div>
 
 <div id=page>
- <div class=sidebar id=sponsor>
+ <aside id=sponsor>
   <h2> Sponsor </h2>
- </div>
+ </aside>
  
- <div class=sidebar id=board>
-  <h2> Community Board </h2>
-	<div class="center comandi">
-	<img alt="" class=rigadimensione src=rigasolaCB.png><br>
-	<ul class="center FlyOut DropDown">
-	 <li><a href=#> Ordina </a>
-	  <ul class=bubble>
-		  <li><a href=#>Per data</a><a class=freccia> </a>
-			<li><a href=#>Per nome</a><a class=freccia> </a>
-		  <li><a href=#>Per visite</a><a class=freccia> </a>
-		  <li><a href=#>Per voto</a><a class=freccia> </a>
-		</ul>
-		
-	 <li><a href=#> Filtra </a>
-	  <ul class=bubble>
-			<li><a href=#>Nessun filtro</a>
-		  <li><a href=#>Per voto</a>
-		  <li><a href=#>Per data</a>
-		</ul>
-	</ul><br>
-  <img alt="" class=rigadimensione src=rigasolaCB.png>
-	</div>
- </div>
-
  <table id=bacheca class="sortable center">
-  <caption class="center comandi"><h1>Bacheca Eventi</h1>
-	<img alt="" class=rigadimensione src=rigasolaB.png><br>
-	<ul class="center FlyOut DropDown">
-	 <li><a href=#> Ordina </a>
-	  <ul class="bubble bachecaul">
-		  <li><table><thead><tr>
-        <th>Per nome<a class=freccia> </a>
-        <th>Per data<a class=freccia> </a>
-        <th>visti<a class=freccia> </a>
-        <th>Voti<a class=freccia> </a>
-      </table>
-		</ul>
-		
-	 <li><a href=#> Filtra </a>
-	  <ul class="bubble bachecaul">
-			<li><a href=#>Nessun filtro</a>
-		  <li><a href=#>Per voto</a><ul><li><input type=text class=slider-range></ul>
-		  <li><a href=#>Per data</a>
-		</ul>
-	</ul><br>
-  <img alt="" class=rigadimensione src=rigasolaB.png>	
+  <caption class="center comandi">
+	  <h1>Bacheca Eventi</h1>
+		<img alt="" class=rigadimensione src=rigasolaB.png><br>
+		<ul class="center FlyOut DropDown">
+		 <li><a href=#> Ordina </a>
+			<ul class="bubble bachecaul">
+				<li><table><thead><tr>
+					<th>Per nome<a class=freccia> </a>
+					<th>Per data<a class=freccia> </a>
+					<th>visti<a class=freccia> </a>
+					<th>Voti<a class=freccia> </a>
+				</table>
+			</ul>
+			
+		 <li><a href=#> Filtra </a>
+			<ul class="bubble bachecaul">
+				<li><a href=#>Nessun filtro</a>
+				<li><a href=#>Per voto</a><ul><li><input type=text class=slider-range></ul>
+				<li><a href=#>Per data</a>
+			</ul>
+		</ul><br>
+		<img alt="" class=rigadimensione src=rigasolaB.png>	
 	<tbody>
 <tr><td class="argomento salutepost">Salute
 	<td class=titolopost><a>Titolo del post</a>
@@ -274,6 +251,63 @@ jQuery(function($) {
 	<td class=visite>235 <img alt="" width=20 height=13 src=visualverde.png>
 	<td class=voto><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png>
 	<td class=nomedata>Organizzatore - 12/12/12
+<tr><td class="argomento socialepost">Sociale
+	<td class=titolopost><a>Titolo del post</a>
+	<td class=periodo> 01/01/01 - 02/02/02
+	<td><img class=fotopost width=180 height=90 src=foto.jpg>
+	<td class=descrizione>Prime righe della descrizione dell'evento o breve sintesi dell'evento
+	<td class=visite>235 <img alt="" width=20 height=13 src=visualverde.png>
+	<td class=voto><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png>
+	<td class=nomedata>Organizzatore - 12/12/12
+<tr><td class="argomento socialepost">Sociale
+	<td class=titolopost><a>Titolo del post</a>
+	<td class=periodo> 01/01/01 - 02/02/02
+	<td><img class=fotopost width=180 height=90 src=foto.jpg>
+	<td class=descrizione>Prime righe della descrizione dell'evento o breve sintesi dell'evento
+	<td class=visite>235 <img alt="" width=20 height=13 src=visualverde.png>
+	<td class=voto><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png>
+	<td class=nomedata>Organizzatore - 12/12/12
+<tr><td class="argomento socialepost">Sociale
+	<td class=titolopost><a>Titolo del post</a>
+	<td class=periodo> 01/01/01 - 02/02/02
+	<td><img class=fotopost width=180 height=90 src=foto.jpg>
+	<td class=descrizione>Prime righe della descrizione dell'evento o breve sintesi dell'evento
+	<td class=visite>235 <img alt="" width=20 height=13 src=visualverde.png>
+	<td class=voto><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png>
+	<td class=nomedata>Organizzatore - 12/12/12
+<tr><td class="argomento socialepost">Sociale
+	<td class=titolopost><a>Titolo del post</a>
+	<td class=periodo> 01/01/01 - 02/02/02
+	<td><img class=fotopost width=180 height=90 src=foto.jpg>
+	<td class=descrizione>Prime righe della descrizione dell'evento o breve sintesi dell'evento
+	<td class=visite>235 <img alt="" width=20 height=13 src=visualverde.png>
+	<td class=voto><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png><img alt="" width=16 height=13 src=stellavoto.png>
+	<td class=nomedata>Organizzatore - 12/12/12
 </table>
-<div style="clear:both;"></div>
+
+ <aside id=board>
+  <h2> Community Board </h2>
+	<div class="center comandi">
+	<img alt="" class=rigadimensione src=rigasolaCB.png><br>
+	<ul class="center FlyOut DropDown">
+	 <li><a href=#> Ordina </a>
+	  <ul class=bubble>
+		  <li><a href=#>Per data</a><a class=freccia> </a>
+			<li><a href=#>Per nome</a><a class=freccia> </a>
+		  <li><a href=#>Per visite</a><a class=freccia> </a>
+		  <li><a href=#>Per voto</a><a class=freccia> </a>
+		</ul>
+		
+	 <li><a href=#> Filtra </a>
+	  <ul class=bubble>
+			<li><a href=#>Nessun filtro</a>
+		  <li><a href=#>Per voto</a>
+		  <li><a href=#>Per data</a>
+		</ul>
+	</ul><br>
+  <img alt="" class=rigadimensione src=rigasolaCB.png>
+	</div>
+ </aside>
+
+  <p>credits</p>
 </div>
