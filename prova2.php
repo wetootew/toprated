b1a89de1c334982e9065e6a24eb03066f08a6df8
wetootew <x.x@aol.it>
2014-09-13 14:13:18 +0200

    Update prova2.php

--- prova2.php
+++ prova2.php
@@ -50,12 +50,16 @@ if ($session) {
      };
 
    // Get Facebook Connect JS and append it to the DOM
-   (function() {
-   var e = document.createElement('script'); 
-    e.async = true;
-    e.src = document.location.protocol+'//connect.facebook.net/en_US/all.js#xfbml=1';
-    document.getElementById('fb-root').appendChild(e);
-   }());
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 
   function fbLoginCheck(response){
      if(response.status != 'unknown'){
