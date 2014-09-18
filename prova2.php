cce4d3acc373c3fd5841d15efc352b95f96dba1a
wetootew <x.x@aol.it>
2014-09-18 18:58:16 +0200

    Update prova2.php

--- prova2.php
+++ prova2.php
@@ -31,7 +31,6 @@ if ($session){
     ))->execute()->getGraphObject(GraphUser::className());
 
     echo "Name: " . $user_profile->getName();
-    echo "city: " . $user_profile->getCity();
     echo "email: " . $user_profile['email'];
   echo '<pre>' . print_r( $user_profile, 1 ) . '</pre>';
   
