996f85e793f074953b965f932235f0c853a01e39
wetootew <x.x@aol.it>
2014-09-23 22:06:27 +0200

    Update prova2.php

--- prova2.php
+++ prova2.php
@@ -1,7 +1,8 @@
 <?php 
 $appId = '1439231382984557';
-//if (session_status() == PHP_SESSION_NONE) {
- session_start(); 
+if ( session_status() == PHP_SESSION_NONE ) {
+    session_start();
+}
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
  require_once __DIR__ . '/vendor/autoload.php';
