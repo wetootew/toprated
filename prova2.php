46998d3237953ab99e987f3d438e446ea925a580
wetootew <x.x@aol.it>
2014-09-23 19:05:01 +0200

    Update prova2.php

--- prova2.php
+++ prova2.php
@@ -16,18 +16,7 @@ if (session_status() == PHP_SESSION_NONE) {
   $appId = '1439231382984557';
  FacebookSession::setDefaultApplication($appId,'0a6b44656cebac45c3c6f4fd62aabbca');
  
- $helper = new FacebookJavaScriptLoginHelper();
- try { $session = $helper->getSession(); }
- catch(\Exception $e) { die 'ex: ' . $e->getCode().' '. $e->getMessage();} 
- if (isset($session) && $session) {
-     $user_profile = (new FacebookRequest($session, 'GET', '/me'
-     ))->execute()->getGraphObject(GraphUser::className());
- 
-     echo "Name: " . $user_profile->getName();
-     echo "<br>email: " . $user_profile->getProperty('email');
-   echo '<pre>' . print_r( $user_profile, 1 ) . '</pre>';
-   die;
- }
+
 }
 ?>
 <!DOCTYPE html> 
