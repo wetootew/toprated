f6607a3d2a6ccbdbf3d0dc12eaf9f85059c2456a
wetootew <x.x@aol.it>
2014-10-19 19:55:24 +0200

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -1,66 +1,50 @@
-<div id="fb-root"></div>
-<script>
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&cookie=1&satus=1&appId=1439231382984557&version=v2.1";
-  fjs.parentNode.insertBefore(js, fjs);
-  
-  FB.Event.subscribe('auth.login', function(response) {
-  alert('we')
-    FB._oauth = false;
-    FB.Cookie.setEnabled(true);
-    FB.Auth.setSession(response.authResponse, response.status);
-    FB._oauth = true;
-    window.location.reload();
-});
-}(document, 'script', 'facebook-jssdk'));
-
-
-function statusChangeCallback(response) {
-  if (response.status === 'connected'){
-      FB.api('/me', function(user) {
-    alert(user.name+ ', ' + user.email);
-    });
-  }
-}
-
-function checkLoginState() {FB.getLoginStatus(function(response) {statusChangeCallback(response);});};
-
-
-</script>
-<?php
-use Facebook\HttpClients\FacebookHttpable;
-use Facebook\HttpClients\FacebookCurl;
-use Facebook\HttpClients\FacebookCurlHttpClient;
-use Facebook\Entities\AccessToken;
-use Facebook\Entities\SignedRequest;
+<?php 
 use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookJavaScriptLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\FacebookResponse;
 use Facebook\FacebookSDKException;
 use Facebook\FacebookRequestException;
-use Facebook\FacebookOtherException;
 use Facebook\FacebookAuthorizationException;
 use Facebook\GraphObject;
-use Facebook\GraphSessionInfo;
-use Facebook\FacebookSignedRequestFromInputHelper; // added in v4.0.9
-use Facebook\FacebookJavaScriptLoginHelper;
+use Facebook\GraphUser;
+if ( session_status() == PHP_SESSION_NONE ) {
+ session_start();
+ require_once __DIR__ . '/vendor/autoload.php';
+ error_reporting(E_ALL); ini_set('display_errors', 1);
  
-FacebookSession::setDefaultApplication('1439231382984557', '0a6b44656cebac45c3c6f4fd62aabbca');
-$helper = new FacebookJavaScriptLoginHelper('1439231382984557');
-try {
-    $session = $helper->getSession();
-} catch(FacebookRequestException $ex) {
-    echo 'facebook error';
-    // When Facebook returns an error
-} catch(\Exception $ex) {
-    // When validation fails or other local issues
-    echo 'login error';
+ FacebookSession::setDefaultApplication('1439231382984557','0a6b44656cebac45c3c6f4fd62aabbca');
+ $helper = new FacebookJavaScriptLoginHelper();
+ try { $session = $helper->getSession();}
+ catch (Exception $e) { echo 'ex: ' . $e->getCode().' '. $e->getMessage();}
+ if (isset($session) && $session) {
+     $user_profile = (new FacebookRequest($session, 'GET', '/me'
+     ))->execute()->getGraphObject(GraphUser::className());
+     //save to db $user_profile->getProperty('email'); print_r( $user_profile, 1 )
+     exit "Ciao, " . $user_profile->getName();
+ }
+ //die "error";
 }
-if ($session) echo "logged in!";
 ?>
-<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-</fb:login-button>
-<div class="fb-login-button" onlogin="checkLoginState()" scope="public_profile,email" data-max-rows="1" data-size="icon" data-show-faces="false" data-auto-logout-link="true"></div>
+<!DOCTYPE html> 
+<title>Boh</title>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<div id="fb-root"></div>
+<script>
+ window.fbAsyncInit = function(){FB.init({appId:'1439231382984557', status:1, cookie:1, xfbml:1, oauth:1})};
+
+ (function() {
+  var e = document.createElement('script'); e.async = true;
+  e.src = '//connect.facebook.net/en_US/all.js';
+  document.getElementById('fb-root').appendChild(e);
+ }());
+
+ function fbLoginCheck(response){
+  if(response.status != 'unknown')
+   jQuery.ajax('<?php echo basename(__FILE__); ?>').done(function(data){alert(data);});
+ }
+ 
+ function checkLoginState() { FB.getLoginStatus(function(response) { fbLoginCheck(response); }); }
+</script>
+<div class="fb-login-button" onlogin="checkLoginState()" scope="public_profile,user_about_me,user_location,email" data-size="icon"></div>
