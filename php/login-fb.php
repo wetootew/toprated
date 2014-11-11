3b525ba406ed52463a4457a5baa2914655210f48
wetootew <x.x@aol.it>
2014-11-11 17:08:18 +0100

    Update login-fb.php

--- php/login-fb.php
+++ php/login-fb.php
@@ -1,5 +1,33 @@
 
-<!DOCTYPE html> 
+<?php 
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookJavaScriptLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
+if ( session_status() == PHP_SESSION_NONE ) {
+ session_start();
+ error_reporting(E_ALL); ini_set('display_errors', 1);
+ require_once __DIR__ . '../vendor/autoload.php';
+ 
+ FacebookSession::setDefaultApplication('1439231382984557','0a6b44656cebac45c3c6f4fd62aabbca');
+ $helper = new FacebookJavaScriptLoginHelper();
+ try { $session = $helper->getSession();}
+ catch (Exception $e) { echo 'ex: ' . $e->getCode().' '. $e->getMessage();}
+ if (isset($session) && $session) {
+     $user_profile = (new FacebookRequest($session, 'GET', '/me'
+     ))->execute()->getGraphObject(GraphUser::className());
+     //save to db $user_profile->getProperty('email'); print_r( $user_profile, 1 )
+     echo "Name: " . $user_profile->getName();
+ }
+ //die "error";
+}
+?><!DOCTYPE html> 
 <title>Boh</title>
  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <div id="fb-root"></div>
