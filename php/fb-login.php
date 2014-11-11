ad36dd73bb1ae4ec9c2ba0a3867f695175c10551
wetootew <x.x@aol.it>
2014-11-11 12:56:23 +0100

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -21,7 +21,7 @@ if ( session_status() == PHP_SESSION_NONE ) {
  if (isset($session) && $session) {
   $user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
      //save to db $user_profile->getProperty('email'); print_r( $user_profile, 1 )
-     exit "Ciao, " . $user_profile->getName();
+  echo "Ciao, " . $user_profile->getName();
  }
 }
 ?>
