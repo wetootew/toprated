3e64152ba5d681b6ec115f6541d375cdf8ff33bd
wetootew <x.x@aol.it>
2014-11-18 18:19:12 +0100

    Update fb-login.php

--- php/fb-login.php
+++ php/fb-login.php
@@ -22,29 +22,27 @@ if ( session_status() == PHP_SESSION_NONE ) {
      $user_profile = (new FacebookRequest($session, 'GET', '/me'
      ))->execute()->getGraphObject(GraphUser::className());
      //save to db $user_profile->getProperty('email'); print_r( $user_profile, 1 )
-     exit("Ciao: " . $user_profile->getName());
+     exit( "Ciao, " . $user_profile->getName();
  }
- die("login error");
+ die ("fb login error");
 }
 ?>
+<!DOCTYPE html> 
+<title>Boh</title>
  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <div id="fb-root"></div>
-<!--div id="fb-login"-->
 <script>
  window.fbAsyncInit = function(){FB.init({appId:'1439231382984557', status:1, cookie:1, xfbml:1, oauth:1})};
-
  (function() {
   var e = document.createElement('script'); e.async = true;
   e.src = '//connect.facebook.net/it_IT/all.js';
   document.getElementById('fb-root').appendChild(e);
  }());
-
  function fbLoginCheck(response){
   if(response.status != 'unknown')
-   jQuery.ajax('/php/fb-login.php').done(function(data){alert(data + ' ' $('#fb-login').html()); $('#fb-login').html(data);});
+   jQuery.ajax('<?php echo basename(__FILE__); ?>').done(function(data){alert(data);});
  }
  
  function checkLoginState() { FB.getLoginStatus(function(response) { fbLoginCheck(response); }); }
 </script>
 <div class="fb-login-button" onlogin="checkLoginState()" scope="public_profile,user_about_me,user_location,email" data-size="medium"></div>
-<!--/div-->
